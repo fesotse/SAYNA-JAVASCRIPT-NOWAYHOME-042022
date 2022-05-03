3b8d4fde6839eb13e0c533cd5406b74f6df948b3
mashiyyat15 <andriambolatsiory@gmail.com>
2022-05-03 15:41:44 +0300

    button over done

---
+++ assets/script/index.js
@@ -0,0 +1,34 @@
+function changebtn1(obj){
+    obj.style.color = "var(--white-ad-color) ";
+    obj.style.fontWeight = "bold";
+    obj.style.backgroundColor = "var(--primary-ad-color)";
+    obj.style.boxShadow = "0px 10px 22px var(--white-ad-color)";
+}
+function returnbtn1(obj){
+    obj.style.color = "var(--primary-ad-color)";
+    obj.style.fontWeight = "normal";
+    obj.style.backgroundColor = "var(--white-ad-color)";
+    obj.style.boxShadow = "none";
+}
+
+function changebtn2(obj){
+    obj.style.color = "var(--primary-ad-color)";
+    obj.style.fontWeight = "bold";
+    obj.style.backgroundColor = "var(--white-ad-color)";
+    obj.style.boxShadow = "0px 10px 22px var(--white-ad-color)";
+    
+}
+function returnbtn2(obj){
+    obj.style.color = "var(--white-ad-color)";
+    obj.style.fontWeight = "normal";
+    obj.style.backgroundColor = "transparent";
+    obj.style.boxShadow = "none"
+}
+function logohover(obj){
+    object.style.filter= "invert(1)"
+    // obj.style.color = "var(--white-ad-color)";
+}
+
+function logooff(obj){
+    obj.style.backgroundColor = "black";
+}

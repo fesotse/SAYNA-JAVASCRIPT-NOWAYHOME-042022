9ac17075dad72d655d47439317f71cb93434e2f8
mashiyyat15 <andriambolatsiory@gmail.com>
2022-05-05 13:58:42 +0300

    scroll function done

--- assets/script/index.js
+++ assets/script/index.js
@@ -38,9 +38,29 @@ function footerretourn(obj){
     obj.style.color = "black";
 }
 $(".boxh").ready(function(){
-    $(".boxh").ready(function(){
-        $(".boxh").animate({left:'3'},{duration:3000});
-        $(".description").fadeOut(10, "swing");
-        $(".description").fadeIn(7000, "linear");
+    $(".boxh").animate({left:'3'},{duration:2000});
+    $(".description").hide().fadeIn(9000, "linear");
+    $(".marvel").hide().fadeIn(7000, "linear");
+    $(".marvel").animate({left:'36'},{duration:2000});
+    // $('.marvel').hide().fadeIn('slow').animate({ right: "-10px" }, 1200);
+})
+// mise en place de la fonction pour le défilement de spiderman.
+const scroll= document.querySelector(".scroller");
+// scrolleffect for colorspiderman and black spiderman
+$(document).ready(function(){
+    $(window).scroll(function(){
+        let scroll = $(window).scrollTop();
+        if (scroll>275.20001220703125 && scroll<1840 ){
+            $(".scroller").css('top',scroll - 345 +'px');
+            scroll<1650 ? $(".scroller").css('z-index', 1): $(".scroller").css('z-index', -1);
+        }
+        else{
+            if(scroll>1840 && scroll<2396){
+                $(".blackspiderman").css("top",scroll - 1855 + "px");
+            }
+            $(".blackspiderman").css('top')
+            $(".scroller").css('top');
+        }
+        console.log(scroll)
     })
 })
